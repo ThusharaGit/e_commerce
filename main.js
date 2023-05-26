f32cf44c77313bee04f03fa15f07e1ef1791b4fd
ThusharaGit <thushara.weeraparakrama@gmail.com>
2023-05-26 11:09:51 +0200

    slider done

--- main.js
+++ main.js
@@ -21,12 +21,43 @@ function toggleHamburgerMenu() {
 fetch('https://dummyjson.com/products')
 .then(response => response.json())
 .then(data => {
-   const products = rawData.products;
-   const firstProduct = products[25];
-   const productPhotos = firstProduct.images;
-   console.log('Product photos:', productPhotos);
+   const products = data.products;
+   const product25 = products[25];
+   const product25photos = product25.images;
+   renderSlider(product25photos);
 })
 
-.catch(error => {
-    console.log(error)
-});
+ .catch(error => {
+     console.log(error)
+ });
+
+function renderSlider(photos) {
+    const slider = document.getElementById('slider');
+
+    photos.slice(0,4).forEach((photoUrl) => {
+    const image = document.createElement('img');
+    image.src = photoUrl;
+    slider.appendChild(image);
+    });
+    
+    const pagination = document.getElementById('pagination');
+
+    pagination.addEventListener('click', handlePaginationClick);
+
+    function handlePaginationClick(event) {
+      const clickedDot = event.target;
+      if(clickedDot.classList.contains('page-dot')){
+          const index = parseInt(clickedDot.dataset.index);
+          scrollToIndex(index);
+      } 
+    }
+    
+    function scrollToIndex(index) {
+        const scrollAmount = index * slider.offsetWidth;
+        slider.scrollTo({
+            left: scrollAmount,
+            behavior: 'smooth',
+        });
+    }
+
+}
